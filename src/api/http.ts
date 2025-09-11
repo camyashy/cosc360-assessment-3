a54ed6767fe0c6a54be5a9ac907416021c23b494
Camillie Ashmore <cashmore@myune.edu.au>
2025-09-11 21:30:14 +1000

    Fixed local deployment

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,5 +1,5 @@
 //const API_URL = import.meta.env.PROD ? import.meta.env.VITE_API_URL : '';
-const API_URL = import.meta.env.VITE_API_URL || '';
+const API_URL = import.meta.env.VITE_API_URL || '/api';
 
 function getToken(): string | null {
     return localStorage.getItem("token");
