37f24be2b54f456a13127ca3c6c9575a2196314b
Camillie Ashmore <cashmore@myune.edu.au>
2025-09-11 21:10:42 +1000

    Attempt to remove the trailing / to get postlist page to load on digital ocean

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -5,27 +5,27 @@ import type { ApiResponse } from "../types/Api";
 export const PostsAPI = {
 
     list: async (): Promise<Post[]> => {
-        const response: ApiResponse<Post[]> = await api.get('/api/');
+        const response: ApiResponse<Post[]> = await api.get('');
         return response.data;
     },
 
     get: async (id: number): Promise<Post> => {
-        const response: ApiResponse<Post> = await api.get(`/api/post/${id}`);
+        const response: ApiResponse<Post> = await api.get(`/post/${id}`);
         return response.data;
     },
 
     create: async (data: Pick<Post, "title" | "content" | "category_id">): Promise<Post> => {
-        const response: ApiResponse<Post> = await api.post('/api/post/create', data);
+        const response: ApiResponse<Post> = await api.post('/post/create', data);
         return response.data;
     },
 
     update: async (id: number, data: Pick<Post, "title" | "content" | "category_id">): Promise<Post> => {
-        const response: ApiResponse<Post> = await api.put(`/api/post/edit/${id}`, data);
+        const response: ApiResponse<Post> = await api.put(`/post/edit/${id}`, data);
         return response.data;
     },
 
     remove: async (id: number): Promise<ApiResponse<string>> => {
-        return await api.delete(`/api/post/delete/${id}`);
+        return await api.delete(`/post/delete/${id}`);
     }
 
 };
